7fcd12c14eac6e25096dcc0a2aac31daf2523f0a
tux <tux@elementaryDansTaGueule.(none)>
2013-10-23 20:36:35 +0200

    problem 1

--- prob1.ml
+++ prob1.ml
@@ -1,6 +1,6 @@
 (* retourne la somme de tous les multiples de 3 ou de 5 inférieur ou égale à mille *)
 let multiple3_5 =
-  let rec local = fun k acc -> 
+  let rec local = fun k acc ->
     if k == 1000 then acc
     else
       if (k mod 3 == 0) || (k mod 5 == 0) then local (k + 1) (acc + k)
