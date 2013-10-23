066b667ce87f7c0ca964f6ec38d75efbe34669a8
adn44 <adrien.bodineau@hotmail.fr>
2013-10-23 20:53:42 +0200

    problem 4

--- prob4.ml
+++ prob4.ml
@@ -1,5 +1,6 @@
 open List;;
 
+(* make a list from an integer, example 123 -> [1;2;3] *)
 let list_of_int = fun n ->
   let rec local = fun k acc ->
     match k with
@@ -7,6 +8,12 @@ let list_of_int = fun n ->
       |_ -> local (k/10) ((k mod 10)::acc)
   in local n [];;
 
+
+(* check if two lists are equals or not, example:
+*  [1;2;3] -> [1;2;3]   -> true
+*  [1;2;3] -> [1;3;2]   -> false
+*  [1;2;3] -> [1;2;3;4] -> false
+*)
 let liste_egale = fun l ll ->
   if length l != length ll then false
   else 
@@ -16,9 +23,16 @@ let liste_egale = fun l ll ->
       else  local (i+1) bool
     in local 0 true;;
 
-let est_palindrome = 
+
+(* return true if the number n is a palindrom, false otherwise *)
+let est_palindrome = fun n -> if liste_egale (list_of_int n) (rev (list_of_int n)) then true else false;;
+
+
+(* return the largest palindrom number resulting on the product of 2 numbers of
+ * 3 digits *)
+let palindrome_max = 
   let rec local = fun n k maxpal ->
-    if liste_egale (list_of_int (n*k)) (rev (list_of_int (n*k))) then local n (k-1) (max maxpal (n*k))
+    if est_palindrome (n*k) then local n (k-1) (max maxpal (n*k))
     else if n == 100 && k == 100 then maxpal
     else if k == 100 then local (n-1) 999 maxpal
     else local n (k-1) maxpal
